b1766a21ae5fe2da4119ee17b852d7d40104a382
Vinicius-AMM <aurelioviniciusprofissional@gmail.com>
2025-02-21 10:31:20 -0300

    initial commit

---
+++ freelancer-management-api/src/test/java/com/manager/freelancer_management_api/FreelancerManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manager.freelancer_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelancerManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

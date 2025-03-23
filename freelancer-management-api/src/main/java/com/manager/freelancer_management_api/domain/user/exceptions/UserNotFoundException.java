bc3c537daf748112a9497670c34a033d3ec9373c
Vinicius-AMM <aurelioviniciusprofissional@gmail.com>
2025-03-23 09:22:08 -0300

    feat: add UserNotFoundException class

---
+++ freelancer-management-api/src/main/java/com/manager/freelancer_management_api/domain/user/exceptions/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.manager.freelancer_management_api.domain.user.exceptions;
+
+public class UserNotFoundException extends RuntimeException{
+    public UserNotFoundException() {
+        super("User not found.");
+    }
+}

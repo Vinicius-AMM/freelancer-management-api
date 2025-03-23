abf4b14b879ee923f27435d155b0635193c2ae6a
Vinicius-AMM <aurelioviniciusprofissional@gmail.com>
2025-03-23 09:12:26 -0300

    Feat: add KeyLoadException for key loading errors

---
+++ freelancer-management-api/src/main/java/com/manager/freelancer_management_api/exceptions/KeyLoadException.java
@@ -0,0 +1,7 @@
+package com.manager.freelancer_management_api.exceptions;
+
+public class KeyLoadException extends RuntimeException{
+    public KeyLoadException(String message) {
+        super(message);
+    }
+}

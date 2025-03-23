7a9620468ebe574af988e42993af0dd18e579ec3
Vinicius-AMM <aurelioviniciusprofissional@gmail.com>
2025-03-23 09:13:28 -0300

    feat: add PublicKeyLoadException for public key loading errors

---
+++ freelancer-management-api/src/main/java/com/manager/freelancer_management_api/exceptions/PublicKeyLoadException.java
@@ -0,0 +1,7 @@
+package com.manager.freelancer_management_api.exceptions;
+
+public class PublicKeyLoadException extends KeyLoadException {
+    public PublicKeyLoadException(String message) {
+        super(message);
+    }
+}

0a4fbb1f5b551c8bfb9094c7b5e24a3c67f556ce
Vinicius-AMM <aurelioviniciusprofissional@gmail.com>
2025-03-23 09:15:44 -0300

    feat: add AuthenticationService interface

---
+++ freelancer-management-api/src/main/java/com/manager/freelancer_management_api/services/AuthenticationService.java
@@ -0,0 +1,11 @@
+package com.manager.freelancer_management_api.services;
+
+import com.manager.freelancer_management_api.domain.user.dtos.LoginDTO;
+import com.manager.freelancer_management_api.domain.user.dtos.LoginResponseDTO;
+import com.manager.freelancer_management_api.domain.user.dtos.RegisterUserDTO;
+
+public interface AuthenticationService {
+    LoginResponseDTO login(LoginDTO login);
+    void register(RegisterUserDTO registerData);
+    void isLoginValid(LoginDTO login);
+}

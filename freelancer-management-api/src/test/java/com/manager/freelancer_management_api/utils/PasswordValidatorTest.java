a8e969f9a8e90fd4babd25173ea88a5eafaa9ba4
Vinicius-AMM <aurelioviniciusprofissional@gmail.com>
2025-03-24 11:29:49 -0300

    feat: add unit tests for PasswordValidator component

---
+++ freelancer-management-api/src/test/java/com/manager/freelancer_management_api/utils/PasswordValidatorTest.java
@@ -0,0 +1,44 @@
+package com.manager.freelancer_management_api.utils;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class PasswordValidatorTest {
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @InjectMocks
+    private PasswordValidator passwordValidator;
+
+    @Test
+    void validatePassword_validPassword () {
+        String rawPassword = "testpassword";
+        String encodedPassword = "$2a$10$6QWxbg21ndMh/fOtu6tpVOw42Fd0kTYshCZF0RuKrmgL7.6YTbQQS";
+
+        when(passwordEncoder.matches(rawPassword, encodedPassword)).thenReturn(true);
+        assertDoesNotThrow(() -> passwordValidator.validate(rawPassword, encodedPassword));
+
+        verify(passwordEncoder).matches(rawPassword, encodedPassword);
+    }
+
+    @Test
+    void validatePassword_invalidPassword_ThrowsBadCredentialsException() {
+        String rawPassword = "testpassword";
+        String encodedPassword = "$2a$10$6QWxbg21ndMh/fOtu6tpVOw42Fd0kTYshCZF0RuKrmgL7.6YTbQQS";
+
+        when(passwordEncoder.matches(rawPassword, encodedPassword)).thenReturn(false);
+        assertThrows(BadCredentialsException.class, () -> passwordValidator.validate(rawPassword, encodedPassword));
+
+        verify(passwordEncoder).matches(rawPassword, encodedPassword);
+    }
+}

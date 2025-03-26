56ffaf993b17396c5d588707761ed04c550612fc
Vinicius-AMM <aurelioviniciusprofissional@gmail.com>
2025-03-26 09:43:25 -0300

    feat: add InvalidPasswordException class

---
+++ freelancer-management-api/src/main/java/com/manager/freelancer_management_api/domain/user/exceptions/InvalidPasswordException.java
@@ -0,0 +1,9 @@
+package com.manager.freelancer_management_api.domain.user.exceptions;
+
+import org.springframework.security.authentication.BadCredentialsException;
+
+public class InvalidPasswordException extends BadCredentialsException {
+    public InvalidPasswordException(String message) {
+        super(message);
+    }
+}

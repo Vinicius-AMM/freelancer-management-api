c317eff47480c0703740019bcdbbbd5fee760aa4
Vinicius-AMM <aurelioviniciusprofissional@gmail.com>
2025-03-26 09:47:16 -0300

    refactor: add validation annotations

--- freelancer-management-api/src/main/java/com/manager/freelancer_management_api/domain/user/dtos/LoginDTO.java
+++ freelancer-management-api/src/main/java/com/manager/freelancer_management_api/domain/user/dtos/LoginDTO.java
@@ -1,7 +1,13 @@
 package com.manager.freelancer_management_api.domain.user.dtos;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+
 public record LoginDTO(
+        @NotNull(message = "Fill in this field")
+        @Email(message = "Invalid email format")
         String email,
+        @NotNull(message = "Fill in this field")
         String password
 ) {
 }
